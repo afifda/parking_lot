9f1e12b36a84b1dd02597eff30b0c8f7ba792cf1
afif azhar <afif.azhar@aurea.com>
2019-08-31 00:55:43 +0700

    Change App Name To Parking Lot with some functions

---
+++ parking_lot/Constants.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace parking_lot
+{
+    static class Constants
+    {
+        #region input
+        public const string CREATE_PARKING_LOT = "create_parking_lot";
+        public const string PARK = "park";
+        public const string LEAVE = "leave";
+        public const string STATUS = "status";
+        public const string REGISTRATION_NUMBERS_FOR_CARS_WITH_COLOUR = "registration_numbers_for_cars_with_colour";
+        public const string SLOT_NUMBERS_FOR_CARS_WITH_COLOUR = "slot_numbers_for_cars_with_colour";
+        public const string SLOT_NUMBER_FOR_REGISTRATION_NUMBER = "slot_number_for_registration_number";
+        #endregion
+
+        #region output
+        public const string PARKING_LOT_CREATED = "Created a parking lot with {0} slots";
+        public const string SLOT_ALLOCATED = "Allocated slot number: {0}";
+        public const string SLOT_NUMBER_FREE = "Slot number {0} is free";
+        public const string FULL_PARKED = "Sorry, parking lot is full";
+        public const string NOT_FOUND = "Not found";
+        #endregion
+    }
+}
---
+++ parking_lot/parking_lot.cs
@@ -0,0 +1,102 @@
+﻿using parking_lot;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace parking_lot_app
+{
+    class parking_lot
+    {
+        static void Main(string[] args)
+        {
+            string command;
+            if (args == null || args.Length == 0)
+            {
+                command = Console.ReadLine();
+                while (command != "exit")
+                {
+                    DoActionCommand(command);
+                }
+            }
+            else if (args != null && args.Length == 1)
+            {                
+                var file = new StreamReader(args[0]);
+                while ((command = file.ReadLine()) != null)
+                {
+                    DoActionCommand(command);
+                }
+
+                file.Close();
+            }
+            else if (args != null && args.Length != 1)
+            {
+                Console.WriteLine("The input for running the app was incorrect. Please enter the file path.");
+            }
+
+            Console.ReadKey();
+        }
+
+        private static void DoActionCommand(string command)
+        {
+            var splittedCommands = command.Split(' ');
+            switch (splittedCommands[0])
+            {
+                case Constants.CREATE_PARKING_LOT:
+                    CreateParkingLot(splittedCommands);
+                    break;
+                case Constants.PARK:
+                    Park(splittedCommands);
+                    break;
+                case Constants.LEAVE:
+                    Leave(splittedCommands);
+                    break;
+                case Constants.STATUS:
+                    GetStatus(splittedCommands);
+                    break;
+                case Constants.REGISTRATION_NUMBERS_FOR_CARS_WITH_COLOUR:
+                    RegistrationNumbersForCarsWithColour(splittedCommands);
+                    break;
+                case Constants.SLOT_NUMBERS_FOR_CARS_WITH_COLOUR:
+                    SlotNumbersForRegistrationNumber(splittedCommands);
+                    break;
+                case Constants.SLOT_NUMBER_FOR_REGISTRATION_NUMBER:
+                    SlotNumbersForRegistrationNumber(splittedCommands);
+                    break;
+
+            }
+        }
+
+        private static void SlotNumbersForRegistrationNumber(string[] splittedCommands)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void RegistrationNumbersForCarsWithColour(string[] splittedCommands)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void GetStatus(string[] splittedCommands)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void Leave(string[] splittedCommands)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void Park(string[] splittedCommands)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void CreateParkingLot(string[] splittedCommands)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
